179870a122138e645caa60a3f5eb9473d0b07386
Mark Bello <mcbello51286@gmail.com>
2017-12-01 20:06:12 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,11 +13,14 @@ end
 
 def word_substituter(tweet)
   test_array = tweet.split
+  new_array = []
   dictionary_keys = dictionary.keys
   test_array.each do |word|
     if dictionary_keys.include?(word)
-      word = dictionary[word]
+      new_array.push(dictionary[word])
+    else
+      new_array.push(word)
     end
   end
-  test_array.join(" ")
+  new_arry.join(" ")
 end
