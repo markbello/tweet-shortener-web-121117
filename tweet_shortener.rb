7f3042145cbaff1df2bd1258c191be02b76e2751
Mark Bello <mcbello51286@gmail.com>
2017-12-01 19:32:39 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+dictionary = {
+  "hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}
